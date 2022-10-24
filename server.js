4311c961c75fb861abd2a2a5d0006d55333e3401
Grant Dunbar <grantdunbar8@gmail.com>
2022-10-24 17:09:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 http.createServer(function(request, response) {    
    response.writeHead(200, {"Content-Type": "text/html"});
    response.write("<h1>Hello, Node.js!</h1>");    
-   response.write('<p>Grant Dunbar - p73c353</p>');
+   response.write('<h3>Grant Dunbar - p73c353</h3>');
    response.write('<p>Running node server on port 3043!</p>');
    response.end();
 }).listen(3043);
